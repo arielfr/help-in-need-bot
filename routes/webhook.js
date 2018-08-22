541e578351a575fbe0148926adc3349ce1d2dd79
Ariel Rey <ariel.rey@mercadolibre.com>
2018-08-21 21:24:16 -0300

    Parametrization of message for customer chat

--- routes/webhook.js
+++ routes/webhook.js
@@ -14,7 +14,9 @@ const CONGRATS_REPORT = `Thank you for taking the time to help someone in need. 
 const CONGRATS_HELP_NO_LOCATIONS = `There are no people in need around you. Would you love to continue helping? Talk to me again!`;
 const CONGRATS_HELP = `People in your community reported the following locations:`;
 const CONGRATS_RE_TARGETING = `Would you love to continue helping? Talk to me again!`;
-const CONGRATS_LOCATIONS = `Check out all locations reported on our map here: `
+const CONGRATS_LOCATIONS = `Check out all locations reported on our map here: `;
+const CUSTOMER_CHAT_CONGRATS_REPORT = `You need to do it through https://www.facebook.com or the Messenger App`;
+const CUSTOMER_CHAT_CONGRATS_HELP = `Great! Just look on the map the locations of people in need near you`;
 
 /**
  * Verification Token Endpoint
@@ -75,14 +77,9 @@ router.post('/webhook', (req, res) => {
           if (fromCustomerChat) {
             // Set the message depending on the quick reply
             if (quickReply.payload === 'REPORT') {
-              // QUESTION - hlopez - "share location" no está soportado en el chat web? 
-              //                      Si es así, estaría bueno ponerlo (el bot no tiene la culpa digamos)
-              message = `You need to do it through https://www.facebook.com or the Messenger App`;
+              message = CUSTOMER_CHAT_CONGRATS_REPORT;
             } else if (quickReply.payload === 'HELP') {
-              // QUESTION - hlopez - ya estoy divagando, pero ya que en esta versión no nos puede compartir la ubicación (supongo), 
-              //                     nos podría pasar un texto tipo el "neighborhood", "county" or "state" y darle un link a la web
-              //                     con esa location (tipo un redirect de la misma web)
-              message = `Great! Just look on the map the locations of people in need near you`;
+              message = CUSTOMER_CHAT_CONGRATS_HELP;
             }
 
             facebook.sendMessage(senderId, message);
